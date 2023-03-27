6696940f662b2741e31b9665b6eb5ae990dbe3fa
Yusmary Somaza <ysomaza@gmail.com>
2023-03-27 06:20:28 -0400

    Ejercicio 2, listo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,20 @@
 """
 Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 """
+#Debo restar 1 en la edad en cada vuelta.
+
+
+edad = int(input("Escribe tu edad: "))
+edad = edad - 1
+contador = 0 
+
+
+while edad >= 0:
+    edad = edad - 1
+
+    contador += 1
+    
+    print(contador)
+    
+
+
