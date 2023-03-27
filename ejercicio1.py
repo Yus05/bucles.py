f0f0f863224c3962c0c1208b0dcba654e2bb265d
Yusmary Somaza <ysomaza@gmail.com>
2023-03-27 05:46:59 -0400

    Listo, ejercicio 1.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,14 @@
 """
 Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
 """
+
+palabra = input("Escribe una palabra: ")
+contador = 1
+
+while contador <= 10:
+    print(palabra)
+
+    contador = contador + 1
+
+
+
