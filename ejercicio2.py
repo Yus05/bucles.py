8ee30edd91bf631e86f5f700eb62f4701838d6a0
Yusmary Somaza <ysomaza@gmail.com>
2023-03-28 05:29:33 -0400

    Fin del ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,18 +3,35 @@ Escribir un programa que pregunte al usuario su edad y muestre por pantalla todo
 """
 #Debo restar 1 en la edad en cada vuelta.
 
-
 edad = int(input("Escribe tu edad: "))
-edad = edad - 1
-contador = 0 
+contador = 0
 
 
-while edad >= 0:
+while edad >= 1:
     edad = edad - 1
 
     contador += 1
     
     print(contador)
+
+"""
+Explicacion:
+
+2- El contador sera quien me de el resultado de la sumatoria, ira sumando en cada vuelta. 
+
+3- while edad >= 1: -> Mientras la edad sea mayor o igual a 1, ejecuta...
+
+4- edad = edad - 1 dentro de la condicion -> El usuario pone su edad, le resto uno en cada vuelta hasta que deje de cumplir la condicion, o sea cuando deje de ser menor o igual a cero, se para el bucle y me da el return.
+
+5- contador += 1 -> Se va imprimiendo la sumatoria en cada vuelta.
+
+RESUMEN: En el ciclo, resta 1 a la edad y suma uno al contador e imprime. Y asi hasta que la edad deja de ser mayor o igual a 1. 
+
+
+
+
+"""
+
     
 
 
