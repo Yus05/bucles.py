c1d5379d763c9b536ec87e04d0a40274bf615d23
Yusmary Somaza <ysomaza@gmail.com>
2023-04-03 06:58:51 -0400

    Commit del inicio - ejercicio4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,5 @@
 """
 Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas.
 """
+
+#Deberia tomar la secuencia de numeros, voltearla y luego recorrerla. 
