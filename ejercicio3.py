01f10a67b5552314fdf1f1f735727ed6db114ed3
Yusmary Somaza <ysomaza@gmail.com>
2023-04-06 07:11:54 -0400

    Listos los ejercicios 3 y 4

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,8 +2,6 @@
 Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
 """
 
-"""OJO:ME FALTA SEPARARLOS POR ,"""
-
 numero = int(input("Escribe un numero entero positivo: "))
 
 numero = int(numero + 1)
@@ -16,7 +14,19 @@ for valor in rango:
     print(valor)"""
 
 for valor in range(1, numero, 2):
-    print(valor)
+    #print(valor,",")
+    print(valor, end=", ")
+    
+"""
+"end" es un parámetro opcional que se puede pasar a la función de impresión en Python. Este parámetro especifica el carácter o la cadena que se utilizará para finalizar la línea después de imprimir el contenido especificado.
+----------------------------------------------
+El valor predeterminado de end es \n, lo que significa que después de la instrucción print imprimirá una nueva línea. Entonces, simplemente dijo end es lo que desea imprimir después de que se haya ejecutado la instrucción print.
+
+Por ejemplo: - print ("hello",end=" +") imprimirá hello +
+
+El contenido de end se imprime después de lo que desea imprimir. Por defecto, contiene una nueva línea ("\n") pero se puede cambiar a otra cosa, como una cadena vacía.
+
+"""    
 
 #EXPLICACION: Con la funcion range puedo crear una secuencia de numeros enteros, para luego poder iterarlos. Como argumento de la funcion range paso el numero que introduce el usuario, pero antes debo sumarle a ese numero un 1, ya que la funcion range no toma el valor final en cuenta sino el anterior a ese. Con el ciclo for, itero cada uno de los valores del rango. Utilizo 3 argumentos, el 1 para que comience a iterar desde el numero 1, el numero que introdujo el usuario, y el 2 porque son los saltos que se va a dar en cada iteracion. 
 
--- ejercicio4.py
+++ ejercicio4.py
@@ -3,3 +3,13 @@ Escribir un programa que pida al usuario un número entero positivo y muestre po
 """
 
 #Deberia tomar la secuencia de numeros, voltearla y luego recorrerla. 
+
+numero = int(input("Escribe un numero entero positivo: "))
+
+numero = numero + 1
+
+while numero >= 1:
+    numero = numero - 1
+    print(numero, end=",")
+
+#EXPLICACION: Pido el numero al usuario, al numero le sumo un 1 para que al momento en el que el ciclo comience a ejecutar la primera resta, muestre a partir del numero que el usuario introdujo. EN EL CICLO: Mientras el numero sea mayor o igual a 1, ejecuta la resta de 1 al numero y ve imprimiendo cada resta.
